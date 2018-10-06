5c8c9c4fea21feba2d4b3527ae153cc04eb250e7
Yoidea <yoidea.cc@gmail.com>
2018-10-06 17:34:54 +0900

    Add files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kuso',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts':
+            'kuso = kuso.main:main'
+    },
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
